151fc0ecd9999519028d193e1dfd1875ec070ee7
MLGalura <marc_lawrence_c_galura@dlsu.edu.ph>
2024-03-28 14:45:33 +0800

    Final Fix Animators

---
+++ Assets/Scripts/Utility/ChangeMenuOpener.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMenuOpener : MonoBehaviour
+{
+    [SerializeField] private Animator m_BasicData;
+    [SerializeField] private Animator m_AscensionData;
+
+    public void ShowBasicData()
+    {
+        this.m_BasicData.SetBool("Open", true);
+        this.m_AscensionData.SetBool("Open", false);
+    }
+
+    public void ShowAscensionData()
+    {
+        this.m_BasicData.SetBool("Open", false);
+        this.m_AscensionData.SetBool("Open", true);
+    }
+}
